5760c005bbe030b5c7528350b304aa9437243db4
Jmedinabe <jmedinabe@unal.edu.co>
2024-02-27 13:08:44 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicio14/Ejercicio14.java
@@ -0,0 +1,13 @@
+package com.mycompany.ejercicio14;
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+        numero=2;
+        cuadrado=Math.pow(numero,2);
+        cubo=Math.pow(numero,3);
+        System.out.println("El número es: " + numero);
+        System.out.println("El cuadrado del número es: " + cuadrado);
+        System.out.println("El cubo del número es: " + cubo);
+    }
+}
